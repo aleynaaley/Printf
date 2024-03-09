7f525caf9ae4959b7fe0413beb9d8231fa64fa73
Aleyna Erkul <147741440+aleynaaley@users.noreply.github.com>
2024-03-09 11:50:31 +0300

    Update ft_printf_utils.c

--- ft_printf/ft_printf_utils.c
+++ ft_printf/ft_printf_utils.c
@@ -56,12 +56,8 @@ int	ft_address(unsigned long a, int sign)
 
 int	ft_string(char *str)
 {
-	int	i;
-
-	i = -1;
 	if (!str)
 		return (write(1, "(null)", 6));
-	while (str[++i])
-		write(1, &str[i], 1);
-	return (i);
+	else
+		return(write(1, str, ft_strlen(str)));
 }
