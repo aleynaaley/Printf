4d5928d03f7d404c7a3efd6b7fa6f7fc236899b2
Aleyna Erkul <147741440+aleynaaley@users.noreply.github.com>
2024-03-11 09:30:40 +0300

    Add files via upload

---
+++ ft_printf/ft_printf.h
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: alerkul <alerkul@student.42istanbul.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/03/09 15:16:53 by alerkul           #+#    #+#             */
+/*   Updated: 2024/03/11 08:54:14 by alerkul          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_PRINTF_H
+# define FT_PRINTF_H
+
+# include <unistd.h>
+# include <stdbool.h>
+# include <stdarg.h>
+# include <stdlib.h>
+
+int		ft_printf(const char *str, ...);
+int		ft_specifier(va_list arg, char c);
+int		ft_putchar(char a);
+int		ft_string(char *str);
+int		ft_address(unsigned long a, int sign);
+int		ft_hex(unsigned int a, char c);
+int		ft_int(int a);
+int		ft_unsigned(unsigned int a);
+
+bool	ft_specifier_cont(const char *str, int i);
+void	ft_putstr_fd(char *s, int fd);
+char	*ft_itoa(int n);
+size_t	ft_strlen(const char *s);
+
+#endif
---
+++ ft_printf/ft_printf_utils.c
@@ -0,0 +1,74 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf_utils.c                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: alerkul <alerkul@student.42istanbul.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/03/09 15:16:36 by alerkul           #+#    #+#             */
+/*   Updated: 2024/03/11 08:56:38 by alerkul          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ft_printf.h"
+
+int	ft_unsigned(unsigned int a)
+{
+	int	count;
+
+	count = 0;
+	if (a >= 10)
+		count += ft_unsigned(a / 10);
+	write(1, &"0123456789"[a % 10], 1);
+	return (count + 1);
+}
+
+int	ft_int(int a)
+{
+	char	*num;
+	int		chr;
+
+	num = ft_itoa(a);
+	chr = ft_strlen(num);
+	ft_putstr_fd(num, 1);
+	free(num);
+	return (chr);
+}
+
+int	ft_hex(unsigned int a, char c)
+{
+	int	count;
+
+	count = 0;
+	if (a >= 16)
+		count += ft_hex(a / 16, c);
+	if (c == 'X')
+		write(1, &"0123456789ABCDEF"[a % 16], 1);
+	if (c == 'x')
+		write(1, &"0123456789abcdef"[a % 16], 1);
+	return (count + 1);
+}
+
+int	ft_address(unsigned long a, int sign)
+{
+	int	ret;
+
+	ret = 0;
+	if (sign == 1)
+	{
+		ret += write(1, "0x", 2);
+		sign = 0;
+	}
+	if (a >= 16)
+		ret += ft_address(a / 16, 0);
+	write(1, &"0123456789abcdef"[a % 16], 1);
+	return (ret + 1);
+}
+
+int	ft_string(char *str)
+{
+	if (!str)
+		return (write(1, "(null)", 6));
+	else
+		return (write(1, str, ft_strlen(str)));
+}
